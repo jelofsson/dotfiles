fbd596e1cd130c2d28976047fb75b42c1b666552
Jimmi Elofsson <jimmi.elofsson@tiqqe.com>
2023-09-22 14:37:34 +0200

    add flameshot

--- mount.sh
+++ mount.sh
@@ -1,3 +1,4 @@
 mount --bind /home/jimmi/.config/i3 .config/i3
+mount --bind /home/jimmi/.config/flameshot .config/flameshot
 mount --bind /home/jimmi/.runelite .runelite
 mount --bind /home/jimmi/.mangohud .mangohud
