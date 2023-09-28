5c736d1f64b0928196587381b987bb5e2e19b467
Jimmi Elofsson <contact@xrb.se>
2023-09-28 22:54:03 +0200

    add gtk-3.0

--- mount.sh
+++ mount.sh
@@ -1,3 +1,4 @@
+mount --bind /home/jimmi/.config/gtk-3.0 .config/gtk-3.0
 mount --bind /home/jimmi/.config/i3 .config/i3
 mount --bind /home/jimmi/.config/flameshot .config/flameshot
 mount --bind /home/jimmi/.runelite .runelite
